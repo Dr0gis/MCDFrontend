74cf06290ad8c833b18108ec125c6d7255b5cd01
Igor Dr0gis <dr0gissf@gmail.com>
2018-05-14 00:55:39 +0300

    final day commit

--- scripts/script.js
+++ scripts/script.js
@@ -18,29 +18,34 @@
 }*/
 
 var serverUrl = 'http://localhost:9000/';
+var googleMapsApiKey = 'AIzaSyAfdWr8eiluFFBNtPGeMcv3CnLAOk76Wgs';
 
-function registrationRequest(email, password) {
-    var url = serverUrl + 'Registration';
+function registrationRequest(email, login, password) {
+    showProgress()
+    var url = serverUrl + 'RegistrationAdmin';
     var request = $.ajax({
         type: 'POST',
-        data: JSON.stringify({email: email, password: password}),
+        data: JSON.stringify({email: email, login: login, password: password}),
         url: url,
         contentType: 'application/json',
         success: function(result) {
             console.log('Registration done');
             console.log(result);
+            hideProgress()
         },
         error: function() {
             console.log('Registration error');
+            hideProgress()
         }
     });
 }
 
-function authorizationRequest(email, password) {
-    var url = serverUrl + 'Authorization';
+function authorizationRequest(emailOrLogin, password) {
+    showProgress()
+    var url = serverUrl + 'AuthorizationAdmin';
     var request = $.ajax({
         type: 'POST',
-        data: JSON.stringify({email: email, password: password}),
+        data: JSON.stringify({emailOrLogin: emailOrLogin, password: password}),
         url: url,
         contentType: 'application/json',
         success: function(result) {
@@ -48,30 +53,43 @@ function authorizationRequest(email, password) {
             console.log(result);
             saveToken(result);
             showMainContent();
+            hideProgress()
         },
         error: function() {
             console.log('Authorization error');
+            hideProgress()
         }
     });
 }
 
-function getStatisticsRequest() {
+function getStatisticsRequest(token) {
+    showProgress()
     var url = serverUrl + 'Statistics';
     var request = $.ajax({
         type: 'GET',
         data: {
-            param1: "param1",
-            param2: 2
+            token: token
         },
         url: url,
         success: function(result) {
             console.log('Statistics done');
             console.log(result);
+            formUserList(result);
+            hideProgress()
         },
         error: function() {
             console.log('Statistics error');
+            hideProgress()
         }
     });
+}
+
+function showProgress() {
+    progress.isShow = true;
+}
+
+function hideProgress() {
+    setTimeout('progress.isShow = false', 500);
 }
 
 function saveToken(token) {
@@ -86,26 +104,33 @@ function removeToken() {
     return sessionStorage.removeItem('token');
 }
 
+var progress = new Vue({
+    el: '#progress',
+    data: {
+        isShow: false,
+    }
+});
+
 Vue.component('authorization', {
     template: 
         '<div class="content" id="authorization">' +
             '<form @submit.prevent="authorization">' +
-                '<span>Email</span>' +
-                '<input v-model="email" type="email" placeholder="Email">' +
+                '<span>Email or Login</span>' +
+                '<input v-model="emailOrLogin" type="text" placeholder="Email or Login" required>' +
                 '<span>Password</span>' +
-                '<input v-model="password" type="password" placeholder="Password">' +
+                '<input v-model="password" type="password" placeholder="Password" required>' +
                 '<button type="submit">Authorization</button>' +
             '</form>' +
         '</div>',
     data: function () {
         return {
-            email: '',
+            emailOrLogin: '',
             password: ''
         };
     },
     methods: {
         authorization: function () {
-            authorizationRequest(this.email, this.password);
+            authorizationRequest(this.emailOrLogin, this.password);
         }
     }
 });
@@ -115,21 +140,24 @@ Vue.component('registration', {
         '<div class="content" id="registration">' +
             '<form @submit.prevent="registration">' +
                 '<span>Email</span>' +
-                '<input v-model="email" type="email" placeholder="Email">' +
+                '<input v-model="email" type="email" placeholder="Email" required>' +
+                '<span>Login</span>' +
+                '<input v-model="login" type="text" placeholder="Login" required>' +
                 '<span>Password</span>' +
-                '<input v-model="password" type="password" placeholder="Password">' +
+                '<input v-model="password" type="password" placeholder="Password" required>' +
                 '<button type="submit">Registration</button>' +
             '</form>' +
         '</div>',
     data: function () {
         return {
             email: '',
+            login: '',
             password: ''
         };
     },
     methods: {
         registration: function (event) {
-            registrationRequest(this.email, this.password);
+            registrationRequest(this.email, this.login, this.password);
         }
     }
 });
@@ -162,28 +190,99 @@ var boxRegAuth = new Vue({
     }
 });
 
+function formUserList(statistics) {
+    listUsers.items.splice(0,  listUsers.items.length)
+    for (var i = 0; i < statistics.length; i++) {
+        listUsers.items.push(statistics[i].email);
+    }
+}
+
+var listUsers = {
+    items: [],
+    active: -1,
+};
+Vue.component('list', {
+    template: 
+        '<div class="list">' +
+            '<div v-for="(item, index) in items">' +
+                '<div :class="{item: true, active: index == active}" :index="index" @click="setActive(index)">{{item}}</div>' +
+            '</div>' +
+        '</div>',
+    data: function () {
+        return listUsers;
+    },
+    methods: {
+        setActive: function (index) {
+            if (this.active == index) {
+                this.active = -1;
+                hideUserData();
+                return;
+            }
+            this.active = index;
+            showUserData();
+        }
+    }
+});
+
 var mainContent = new Vue({
     el: '#main-content',
     data: {
-        isShow: false
+        isShow: false,
+        isContentShow: true,
+        isProfileShow: false,
+        indexActiveTitle: 1,
+        isUserDataShow: false,
+        isZoneDataShow: true,
+        isEventListShow: true
     },
     methods: {
         logOut: function () {
             removeToken();
             showBoxRegAuth();
-        }
+        },
+        showContent: function () {
+            showContent();
+        },
+        showProfile: function () {
+            showProfile();
+        },
     }
 });
 
 function showMainContent() {
     boxRegAuth.isShow = false;
     mainContent.isShow = true;
-    getStatisticsRequest();
+    getStatisticsRequest(getToken());
 }
 
 function showBoxRegAuth() {
     boxRegAuth.isShow = true;
     mainContent.isShow = false;
+}
+
+function showContent() {
+    mainContent.isContentShow = true;
+    mainContent.isProfileShow = false;
+    mainContent.indexActiveTitle = 1;
+    getStatisticsRequest(getToken());
+}
+
+function showProfile() {
+    mainContent.isContentShow = false;
+    mainContent.isProfileShow = true;
+    mainContent.indexActiveTitle = 2;
+}
+
+function showUserData() {
+    mainContent.isUserDataShow = true;
+    mainContent.isZoneDataShow = false;
+    mainContent.isEventListShow = false;
+}
+
+function hideUserData() {
+    mainContent.isUserDataShow = false;
+    mainContent.isZoneDataShow = true;
+    mainContent.isEventListShow = true;
 }
 
 function startApp() {
